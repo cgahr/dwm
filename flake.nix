ebcaec0c6b4bda69989a906ac479a7af90eca8b2
Constantin Gahr <constantin.gahr@ipp.mpg.de>
2024-03-06 20:08:34 +0100

    make dwm package a flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    in
+    {
+      packages.x86_64-linux.dwm = pkgs.dwm.overrideAttrs (old: {
+        src = ./.;
+        buildInputs = old.buildInputs ++ [ pkgs.pulseaudio ];
+      })
+      ;
+      packages.x86_64-linux.default = self.packages.x86_64-linux.dwm;
+    };
+}
