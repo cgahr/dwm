ddbfe94ffe3bf79e46e6efcfd02900625a558cb1
Constantin Gahr <constantin.gahr@ipp.mpg.de>
2024-03-07 19:48:17 +0100

    start zsh with terminal

--- config.def.h
+++ config.def.h
@@ -64,8 +64,8 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *termcmd[]  = { "st", NULL };
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_background, "-nf", col_foreground, "-sb", col_selection_b, "-sf", col_foreground, NULL };
+static const char *termcmd[]  = { "st", "-e", "zsh", NULL };
 static const char *up_vol[]   = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "+10%",   NULL };
 static const char *down_vol[] = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "-10%",   NULL };
 static const char *mute_vol[] = { "pactl", "set-sink-mute",   "@DEFAULT_SINK@", "toggle", NULL };
